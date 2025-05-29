447e382455227d087abcabb36c7cf0a25f0c5d09
Leonel Subelza <subelza150@gmail.com>
2025-05-29 15:17:22 -0300

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <>
       <main className="flex flex-col max-w-4xl mx-auto px-4 font-[family-name:var(--font-fira-code)]">
+        {/* <div className="absolute left-[0px] z-[-1] w-full h-full background-animation-gradient"></div> */}
         <div className="w-full h-[100dvh] flex flex-col items-center justify-center items-start">
           <h1 className="text-3xl text-gray-600 mb-5">Full Stack Developer</h1>
           <p className="text-center sm:text-left max-w-2xl">
